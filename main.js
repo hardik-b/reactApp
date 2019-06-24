275b26e3ef5bb09393cf4a26234041aaca44f2c8
Hardik Buddhbhatti <thehardik@gmail.com>
2019-06-25 09:21:45 +1000

    Further files added

--- main.js
+++ main.js
@@ -4,11 +4,29 @@ import App from './App.js';
 import AppSetState from './AppSetState.js';
 import AppFindDom from './AppFindDom.js';
 import AppLifeCycle from './AppLifeCycle.js';
+import AppRoute from './AppRoute.js';
+import {Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
+import {Home,About,Contact} from './AppRoute.js';
 
 //ReactDOM.render(<App />, document.getElementById('app'));
 //ReactDOM.render(<AppSetState />, document.getElementById('app'));
 //ReactDOM.render(<AppFindDom />, document.getElementById('app'));
-ReactDOM.render(<AppLifeCycle />, document.getElementById('app'));
+//ReactDOM.render(<AppLifeCycle />, document.getElementById('app'));
 
-setTimeout(() => {
-    ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 10000);
+//setTimeout(() => {
+//    ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 10000);
+
+
+
+    ReactDOM.render((
+        <Router history={browserHistory}>
+           <Route path="/" component = {AppRoute}>
+              <IndexRoute component = {Home} />
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Route path="/home" component = {Home} />
+              <Route path="/about" component = {About} />
+              <Route path="/contact" component = {Contact} />
+           </Route>
+        </Router>
+     ), document.getElementById('app'));
